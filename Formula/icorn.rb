9242d08b589e7862a6c2c90d3466c25658700818
hakota <araki.kenta.db@gmail.com>
2017-07-17 22:54:46 +0900

    Fix icorn.rb

--- Formula/icorn.rb
+++ Formula/icorn.rb
@@ -6,7 +6,7 @@ class Icorn < Formula
   desc ""
   homepage ""
   url "https://github.com/hakota/homebrew-icorn"
-  sha256 "d6e044a72fdc0c0f47093f452037de0fd1b1e151956e0947f63d2aeaaae00bbf"
+  sha256 ""
 
   # depends_on "cmake" => :build
 
